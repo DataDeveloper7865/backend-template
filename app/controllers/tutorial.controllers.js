8deaa98eff4052f7ea562a10e1d15e3845aeb7b2
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-29 12:37:12 -0700

    added findAll method

--- app/controllers/tutorial.controllers.js
+++ app/controllers/tutorial.controllers.js
@@ -29,11 +29,25 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database
 exports.findAll = (req, res) => {
-    
+    const title = req.query.title;
+    var condition = title ? {title: { $regex: new RegExp(title), $options: "i"}} : {};
+
+    Tutorial.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "A message occurred while retrieving tutorials"
+            });
+        });
+
 };
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
 };
 
